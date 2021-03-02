4d99e693d91a67e83e9b6af163adf9d2062281fc
Maryia <mk70123n@pace.edu>
2021-03-01 22:08:25 -0500

    Working on Iperfer.py client

--- project1/src/Iperfer_client.py
+++ project1/src/Iperfer_client.py
@@ -1,6 +1,6 @@
 import socket
 import sys
-import timeit
+import time
 
 if len(sys.argv) > 4 or len(sys.argv) < 4:
     sys.exit('Error: missing or additional arguments')
@@ -14,32 +14,36 @@ ServerAddress = (ServerName, ServerPort)
 
 time_window = float(sys.argv[3]) #elapsed in seconds, [0] is prog name
 
-
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect(ServerAddress)
 #Create client socket
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket: #IPv4, TCP socket
-    #Ask for connection to server, attach the socket directly to the remote address.
-    clientSocket.connect(ServerAddress)
+#with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket: #IPv4$
+    #Ask for connection to server, attach the socket directly to the remote ad$
+    # clientSocket.connect(ServerAddress)
     #set time out
-    clientSocket.settimeout(time_window)
+   # clientSocket.settimeout(time_window)
 #3-way handshake is performed => connection is established
+clientSocket.settimeout(time_window)
+#clientSocket = socket.create_connection(ServerAddress, timeout=time_window)
+starttime = time.time()
 count = 0 #in KB
-while 1: 
+while 1: #and not socket.timeout:
     try:
-        #left-justify=padding message with fixed size '0'
-        message = '0' 
+        #left-justify=padding message uwith fixed size '0'
+        message = '0'
         message = message.ljust(1000, '0')
         count += 1
         #Send message
-        clientSocket.sendall(message.encode('ascii')) #change ascii?
+        clientSocket.sendall(message.encode('utf-8')) #change ascii?
 
     #modified_sent = clientSocket.recvfrom(1000)
-
     except socket.timeout as e:
         break
+    if socket.timeout > 0 and time.time() - starttime >= socket.timeout:      $
+        clientSocket.close()
 
 mb = count/1000
 rate = mb/time_window
-print('The total number of bytes sent (in kilobytes) = {count}'.format(count), end=' ')
-print('The rate at which traffic could be sent (in megabits per second (Mbps)) = {rate}'.format(rate))
 
- 
+print('Sent = {count} KB. Rate = {rate}'.format(count, rate))
+
