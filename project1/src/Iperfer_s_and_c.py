88a602d02eb4adbb4721f2ea8187cbb9b47d0c1b
Maryia <mk70123n@pace.edu>
2021-03-02 19:41:36 -0500

    Temp commit #2

--- project1/src/Iperfer_s_and_c.py
+++ project1/src/Iperfer_s_and_c.py
@@ -70,20 +70,17 @@ if sys.argv[1] == "-s":
 
 elif sys.argv[1] == "-c":
     
-    #Iperfer should operate in client mode
-    #python3 Iperfer.py  -c <server hostname>  <server port>  <time>
-   
-    if len(sys.argv) > 5 or len(sys.argv) < 5:
+    if len(sys.argv) > 4 or len(sys.argv) < 4:
         sys.exit('Error: missing or additional arguments')
-    if int(sys.argv[3]) < 1024 or int(sys.argv[3]) > 65535:
+    if int(sys.argv[2]) < 1024 or int(sys.argv[2]) > 65535:
         sys.exit('Error: port number must be in the range 1024 to 65535')
 
     #Create server
-    ServerName = sys.argv[2]
-    ServerPort = int(sys.argv[3])
+    ServerName = sys.argv[1]
+    ServerPort = int(sys.argv[2])
     ServerAddress = (ServerName, ServerPort)
 
-    time_window = float(sys.argv[4]) #elapsed in seconds, [0] is prog name
+    time_window = float(sys.argv[3]) #elapsed in seconds, [0] is prog name
 
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientSocket.connect(ServerAddress)
@@ -98,11 +95,15 @@ elif sys.argv[1] == "-c":
     while (time.time() - start_time) < time_window:
         try:
             #left-justify=padding message uwith fixed size '0'
+	    #clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	    #clientSocket.connect(ServerAddress)
 	    message = '0'
             message = message.ljust(1000, '0')	
+	    #count += 1 #this count = 1
 	    while (time.time() - start_time) < time_window:
 	       count += 1
        	       clientSocket.sendall(message.encode('utf-8')) #change ascii?
+ 	    #clientSocket.close() 
         except socket.timeout as e:
             break	
     clientSocket.close()
@@ -112,6 +113,7 @@ elif sys.argv[1] == "-c":
     rate = megabit/time_window
     #print("time_window: {}".format(time_window))
     print('Sent = {} KB. Rate = {} Mbps'.format(count, rate))
+
 
 
 
