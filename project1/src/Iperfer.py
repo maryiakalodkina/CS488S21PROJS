8baf5c57d06deb11c602927339e81320e4bc86a8
None <None>
2021-03-06 06:42:46 -0800

    Final working version of the Iperfer

--- project1/src/Iperfer.py
+++ project1/src/Iperfer.py
@@ -3,7 +3,6 @@ import sys
 import time
 
 if sys.argv[1] == "-s":
-    #pass
     #Iperfer should operate in server mode
 
     #python3 Iperfer.py -s <listen port>
@@ -19,7 +18,7 @@ if sys.argv[1] == "-s":
     ServerPort = int(sys.argv[2])
     ServerAddress = (ServerName, ServerPort)
 
- #Creating server socket
+    #Creating server socket
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     #Associating server port number with the socket
@@ -31,37 +30,31 @@ if sys.argv[1] == "-s":
     print('The server is ready to listen')
     #Creating connection socket
     #It is blocked and don't go to while-loop if no one connects
-    Scount = 0 #in Bytes
-    #connection_socket, addr = serverSocket.accept()
+    Serv_count = 0 #in Bytes
+    connection_socket, addr = serverSocket.accept()  #move to while-lo$
     start_time2 = time.time()
+
     while 1:
-      connection_socket, addr = serverSocket.accept()  #move to while-lo$
-      #start_time2 = time.time()
-      while 1:
         #Receiving message from client
-        message = connection_socket.recv(1000)
-      
-      #Scount+=1000
-        if message[0] == 69:
+      message = connection_socket.recv(1000)
+
+      #by sending ascii 69 ('E') we notify the Server that Client closed its socket
+      #so the Server will also shut down      
+      if message[0] == 69:
         #connection_socket.close()
-          stop_time2 = time.time()-start_time2
-          connection_socket.close()
-        #print(stop_time2)
-          break
-        Scount+=1000
-      break
-        #Closing communication with client
-     # connection_socket.close()
-   # print(stop_time2)
-    #stop_time2 = time.time() - start_time2
-    print('Count: {} and StopTime: {}'.format(Scount, stop_time2))
+        stop_time2 = time.time()-start_time2
+        connection_socket.close()
+        break
+      Serv_count+=1000
+
+#    print('Count: {} and StopTime: {}'.format(Scount, stop_time2))
     serverSocket.close()
-    megabit = Scount*0.000008
-    Scount = Scount/1000 #now it's in KB
-    #print("count: {}".format(count))
+
+    megabit = Serv_count*0.000008
+    Serv_count = Serv_count/1000 #now it's in KB
     rate = megabit/stop_time2
-    #print("time_window: {}".format(time_window))
-    print('received = {} KB. rate = {} Mbps'.format(Scount, rate))
+
+    print('received = {} KB. rate = {} Mbps'.format(Serv_count, rate))
 
 else:
 
@@ -71,8 +64,8 @@ else:
     if int(sys.argv[2]) < 1024 or int(sys.argv[2]) > 65535:
       print('Error: port number must be in the range 1024 to 65535')
       sys.exit(1)        
-#Create server
-        #ServerName = sys.argv[2]
+
+     #Create server
     ServerName = sys.argv[1]
     ServerPort = int(sys.argv[2])
     ServerAddress = (ServerName, ServerPort)
@@ -82,40 +75,28 @@ else:
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientSocket.connect(ServerAddress)
 
-#Create client socket
+    #Create client socket
 
     clientSocket.settimeout(time_window)
-#3-way handshake is performed => connection is established
+    #3-way handshake is performed => connection is established
 
     start_time = time.time()
     count = 0 #in B
 
     while (time.time()-start_time) < time_window:
-#      print('Time: {}'.format(clientSocket.settimeout))
  
       size = 1000 
       message = bytearray(size)
 
-      try:
-        clientSocket.sendall(message)
-        count += 1000
-      except clientSocket.timeout as e:
-        break
+      clientSocket.sendall(message)
+      count += 1000
         
-      #  clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      #  clientSocket.connect(ServerAddress)
-      #  clientSocket.sendall(message)
-      #  count += 1000
- 
-   # clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   # clientSocket.connect(ServerAddress)
     messageEOF = 'E'
     clientSocket.send(messageEOF.encode('ascii'))
 
-  #  clientSocket.close()   
-    print(count)
     megabit = count*0.000008
     count = count/1000
     rate = megabit/time_window
+
     print('sent = {} KB. rate = {} Mbps'.format(count, rate))
 
